7de4faedee56ed93a26cce54ac301a3b5035c21d
wagao2018 <gaowa_2008@hotmail.com>
2021-05-16 15:21:40 -0700

    Save progress

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -18,7 +19,13 @@ Set<Marker> initial_markers = {
 };
 
 class MapPageState extends State<MapPage> {
-  GoogleMapController mapController;
+  Completer<GoogleMapController> mapController = Completer(); 
+
+  /* 
+  COMMENT: original code @ line 12
+  was a Completer.
+  */
+
   Set<Marker> _markers = {};
 
   final LatLng _center = const LatLng(37.7785, -122.4056);
@@ -75,99 +82,109 @@ class MapPageState extends State<MapPage> {
       ),
     );
   }
-}
 
-Widget _buildContainer() {
-  return Align(
-    alignment: Alignment.bottomLeft,
-    child: Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0),
-        height: 256.0,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            SizedBox(width: 10.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _boxes(
-                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                  37.7785,
-                  -122.4067,
-                  "Eco Leaf"),
-            ),
-            SizedBox(width: 10.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _boxes(
-                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                  37.7785,
-                  -122.4067,
-                  "Eco Leaf"),
-            ),
-            SizedBox(width: 10.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _boxes(
-                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                  37.7785,
-                  -122.4067,
-                  "Eco Leaf"),
-            ),
-            SizedBox(width: 10.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _boxes(
-                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                  37.7785,
-                  -122.4067,
-                  "Eco Leaf"),
-            )
-          ],
-        )),
-  );
-}
-
-Widget _boxes(String _image, double lat, double long, String cafeName) {
-  return GestureDetector(
-    onTap: () {
-      // _gotLocation(lat, long);
-    },
-    child: Container(
-      child: new FittedBox(
-        child: Material(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+  Widget _buildContainer() {
+    /*
+    Builds the cafeCardList
+    */
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 256.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
             children: <Widget>[
-              Container(
-                child: ClipRRect(
-                  borderRadius: new BorderRadius.only(
-                    topRight: Radius.circular(16),
-                    topLeft: Radius.circular(16),
-                  ),
-                  child: Image(
-                    fit: BoxFit.cover,
-                    width: 343,
-                    height: 105,
-                    image: NetworkImage(_image),
-                  ),
-                ),
+              SizedBox(width: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _boxes(
+                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                    37.7785,
+                    -122.4067,
+                    "Eco Leaf"),
               ),
-              Container(
-                width: 343,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: cafeInfoContainer(cafeName),
-                ),
+              SizedBox(width: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _boxes(
+                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                    37.7785,
+                    -122.4067,
+                    "Eco Leaf"),
+              ),
+              SizedBox(width: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _boxes(
+                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                    37.7785,
+                    -122.4067,
+                    "Eco Leaf"),
+              ),
+              SizedBox(width: 10.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: _boxes(
+                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                    37.7785,
+                    -122.4067,
+                    "Eco Leaf"),
               )
             ],
+          )),
+    );
+  }
+
+  Widget _boxes(String _image, double lat, double long, String cafeName) {
+    return GestureDetector(
+      onTap: () {
+        _gotoLocation(lat, long);
+      },
+      child: Container(
+        child: new FittedBox(
+          child: Material(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: ClipRRect(
+                    borderRadius: new BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      topLeft: Radius.circular(16),
+                    ),
+                    child: Image(
+                      fit: BoxFit.cover,
+                      width: 343,
+                      height: 105,
+                      image: NetworkImage(_image),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 343,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: cafeInfoContainer(cafeName),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  Future<void> _gotoLocation(double lat, double long) async {
+    final GoogleMapController controller = await mapController.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+      bearing: 45.0,)));
+  }
 }
+
 
 Widget cafeInfoContainer(String cafeName) {
   return Padding(
