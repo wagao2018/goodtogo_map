bd27d5b4679c3f4e96ee0fa186da13e77ea089f0
wagao2018 <gaowa_2008@hotmail.com>
2021-05-16 16:30:49 -0700

    ecocafe data model for storing cafe information. Working markers.

--- lib/map.dart
+++ lib/map.dart
@@ -1,250 +1,282 @@
-import 'dart:ui';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MapPage extends StatefulWidget {
   @override
   MapPageState createState() => MapPageState();
 }
 
-Set<Marker> initial_markers = {
-  Marker(markerId: MarkerId('id-2'), position: LatLng(37.7785, -122.4067)),
-  Marker(markerId: MarkerId('id-1'), position: LatLng(37.7885, -122.4056)),
-  Marker(markerId: MarkerId('id-3'), position: LatLng(37.7985, -122.4078)),
-  Marker(markerId: MarkerId('id-4'), position: LatLng(37.7685, -122.4089)),
-  Marker(markerId: MarkerId('id-5'), position: LatLng(37.7585, -122.4090)),
-  Marker(markerId: MarkerId('id-6'), position: LatLng(37.7485, -122.4101)),
-};
-
 class MapPageState extends State<MapPage> {
-  Completer<GoogleMapController> mapController = Completer(); 
-
-  /* 
-  COMMENT: original code @ line 12
-  was a Completer.
-  */
-
+  Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _markers = {};
 
-  final LatLng _center = const LatLng(37.7785, -122.4056);
-
-  void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      _markers = initial_markers;
-    });
-    controller.setMapStyle(Utils.mapStyle);
-    mapController = controller;
+  @override
+  void initState() {
+    super.initState();
   }
-
+    double zoomVal=5.0;
   @override
   Widget build(BuildContext context) {
+    // create markers from initial_cafes
+    // for each cafe in initial_cafes
+      // create a marker for that cafe
+    for (final my_cafe in initial_cafes) {
+      _markers.add(Marker(markerId: my_cafe.markerId, 
+                          position: my_cafe.position, 
+                          infoWindow: InfoWindow(title: my_cafe.name),
+                          icon: BitmapDescriptor.defaultMarkerWithHue(
+                            BitmapDescriptor.hueBlue
+                            )
+                          )
+                  );
+    }
     return Scaffold(
+      appBar: AppBar(
+        // leading: IconButton(
+        //     icon: Icon(FontAwesomeIcons.arrowLeft),
+        //     onPressed: () {
+        //       //
+        //     }),
+        // title: Text("New York"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(FontAwesomeIcons.search),
+              onPressed: () {
+                //
+              }),
+        ],
+      ),
       body: Stack(
         children: <Widget>[
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            markers: _markers,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 14,
-            ),
-          ),
+          _buildGoogleMap(context),
           _buildContainer(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Align(
-                alignment: Alignment.centerRight,
-                child: FloatingActionButton(
-                  onPressed: () {},
-                  child: Icon(
-                    Icons.add,
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FloatingActionButton(
-                  onPressed: () {},
-                  child: Icon(
-                    Icons.map,
-                    // size: 36,
-                  ),
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
   }
-
+  
   Widget _buildContainer() {
-    /*
-    Builds the cafeCardList
-    */
     return Align(
       alignment: Alignment.bottomLeft,
       child: Container(
-          margin: EdgeInsets.symmetric(vertical: 20.0),
-          height: 256.0,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              SizedBox(width: 10.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: _boxes(
-                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                    37.7785,
-                    -122.4067,
-                    "Eco Leaf"),
-              ),
-              SizedBox(width: 10.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: _boxes(
-                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                    37.7785,
-                    -122.4067,
-                    "Eco Leaf"),
-              ),
-              SizedBox(width: 10.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: _boxes(
-                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                    37.7785,
-                    -122.4067,
-                    "Eco Leaf"),
-              ),
-              SizedBox(width: 10.0),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: _boxes(
-                    "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
-                    37.7785,
-                    -122.4067,
-                    "Eco Leaf"),
-              )
-            ],
-          )),
-    );
-  }
-
-  Widget _boxes(String _image, double lat, double long, String cafeName) {
-    return GestureDetector(
-      onTap: () {
-        _gotoLocation(lat, long);
-      },
-      child: Container(
-        child: new FittedBox(
-          child: Material(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  child: ClipRRect(
-                    borderRadius: new BorderRadius.only(
-                      topRight: Radius.circular(16),
-                      topLeft: Radius.circular(16),
-                    ),
-                    child: Image(
-                      fit: BoxFit.cover,
-                      width: 343,
-                      height: 105,
-                      image: NetworkImage(_image),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 343,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: cafeInfoContainer(cafeName),
-                  ),
-                )
-              ],
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                  40.738380, -73.988426,"Gramercy Tavern"),
             ),
-          ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                  40.761421, -73.981667,"Le Bernardin"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg",
+                  40.732128, -73.999619,"Blue Hill"),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Future<void> _gotoLocation(double lat, double long) async {
-    final GoogleMapController controller = await mapController.future;
+  Widget _boxes(String _image, double lat,double long,String restaurantName) {
+    return  GestureDetector(
+        onTap: () {
+          _gotoLocation(lat,long);
+        },
+        child:Container(
+              child: new FittedBox(
+                child: Material(
+                    color: Colors.white,
+                    elevation: 14.0,
+                    borderRadius: BorderRadius.circular(24.0),
+                    shadowColor: Color(0x802196F3),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 180,
+                          height: 200,
+                          child: ClipRRect(
+                            borderRadius: new BorderRadius.circular(24.0),
+                            child: Image(
+                              fit: BoxFit.fill,
+                              image: NetworkImage(_image),
+                            ),
+                          ),),
+                          Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: myDetailsContainer1(restaurantName),
+                          ),
+                        ),
+
+                      ],)
+                ),
+              ),
+            ),
+    );
+  }
+
+  Widget myDetailsContainer1(String restaurantName) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(restaurantName,
+            style: TextStyle(
+                color: Color(0xff6200ee),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          )),
+        ),
+        SizedBox(height:5.0),
+        Container(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                  child: Text(
+                "4.1",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              )),
+              Container(
+                child: Icon(
+                  FontAwesomeIcons.solidStar,
+                  color: Colors.amber,
+                  size: 15.0,
+                ),
+              ),
+              Container(
+                child: Icon(
+                  FontAwesomeIcons.solidStar,
+                  color: Colors.amber,
+                  size: 15.0,
+                ),
+              ),
+              Container(
+                child: Icon(
+                  FontAwesomeIcons.solidStar,
+                  color: Colors.amber,
+                  size: 15.0,
+                ),
+              ),
+              Container(
+                child: Icon(
+                  FontAwesomeIcons.solidStar,
+                  color: Colors.amber,
+                  size: 15.0,
+                ),
+              ),
+              Container(
+                child: Icon(
+                  FontAwesomeIcons.solidStarHalf,
+                  color: Colors.amber,
+                  size: 15.0,
+                ),
+              ),
+               Container(
+                  child: Text(
+                "(946)",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              )),
+            ],
+          )),
+          SizedBox(height:5.0),
+        Container(
+                  child: Text(
+                "American \u00B7 \u0024\u0024 \u00B7 1.6 mi",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              )),
+              SizedBox(height:5.0),
+        Container(
+            child: Text(
+          "Closed \u00B7 Opens 17:00 Thu",
+          style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold),
+        )),
+      ],
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition:  CameraPosition(target: LatLng(37.7685, -122.4089), zoom: 12),
+        onMapCreated: (GoogleMapController controller) {
+          controller.setMapStyle(Utils.mapStyle);
+          _controller.complete(controller);
+        },
+        markers:_markers
+        // markers: {
+        //   newyork1Marker,newyork2Marker,newyork3Marker,gramercyMarker,bernardinMarker,blueMarker
+        // },
+      ),
+    );
+  }
+
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
       bearing: 45.0,)));
   }
 }
 
+class EcoCafe {
+  MarkerId markerId;
+  String name;
+  String pictureUrl;
+  LatLng position;
+  bool acceptsReusableCups;
 
-Widget cafeInfoContainer(String cafeName) {
-  return Padding(
-      padding: const EdgeInsets.only(left: 8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "Open | 0.06MI",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "\u00B7 \u0024\u0024 \u00B7",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Text(
-              cafeName,
-              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            child: Text(
-              "Close at 6 pm",
-              style: TextStyle(fontSize: 12.0),
-            ),
-          ),
-          SizedBox(height: 32.0),
-          Container(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "GO NOW",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-                // onPressed(){}
-                // textAlign: TextAlign.center,
-              ),
-            ],
-          )),
-          SizedBox(height: 16.0),
-        ],
-      ));
+  EcoCafe({
+    this.markerId, 
+    this.name, 
+    this.pictureUrl, 
+    this.position,
+    this.acceptsReusableCups
+  });
 }
+
+List<EcoCafe> initial_cafes = [
+  EcoCafe(markerId: MarkerId('id-1'), name: 'EcoCafe', position: LatLng(37.7785, -122.4067), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: true),
+  EcoCafe(markerId: MarkerId('id-2'), name: 'CoolCafe', position: LatLng(37.7885, -122.4056), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: true),
+  EcoCafe(markerId: MarkerId('id-3'), name: 'WaCafe', position: LatLng(37.7985, -122.4078), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: false),
+  EcoCafe(markerId: MarkerId('id-4'), name: 'IsCafe', position: LatLng(37.7685, -122.4089), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: false),
+  EcoCafe(markerId: MarkerId('id-5'), name: 'ReallyCafe', position: LatLng(37.7585, -122.4090), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: true),
+  EcoCafe(markerId: MarkerId('id-6'), name: 'GoodCafe', position: LatLng(37.7485, -122.4101), pictureUrl: "https://cafeastoria-stpaul.com/wp-content/uploads/2020/08/cafeastoria-interior2.jpg", acceptsReusableCups: true),
+];
+
 
 class Utils {
   static String mapStyle = '''
